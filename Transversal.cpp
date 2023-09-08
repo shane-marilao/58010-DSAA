d9d762119e576def1db430ce0e5a43f9cd8fec36
Shane Marilao <123943222+shane-marilao@users.noreply.github.com>
2023-09-08 08:07:51 +0800

    Add files via upload

---
+++ Transversal.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num[]={1,2,3,4,5,6,7,8,9,10};
+	int i=0;
+	int n=10;
+	cout<<"Traversal Algorithm"<<endl;
+	for(i=0;i<n;i++)
+	{
+		cout<<"num["<<i<<"]="<<i<<endl;
+	}
+	return 0;
+}
